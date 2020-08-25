1f2e1649ba87a7edebac451a0eb57848fe36292a
tompng <tomoyapenguin@gmail.com>
2020-08-26 02:53:05 +0900

    use Complex

--- hanabi.rb
+++ hanabi.rb
@@ -21,39 +21,36 @@ clear = lambda do
   end
 end
 
-gravity = 0.5
-wind = 0.1
+gravity = 0.1+0.5i
 scale = 4.0
-stroke = lambda do |x, y, vx, vy, ax, ay, time|
-  vmax = [vx, vx + ax * time, 2 * vy, (vy + ay * time) / 2].map(&:abs).max
-  step = width * (vmax * time) / scale
+stroke = lambda do |p, v, a, time|
+  v2 = v + a * time
+  vmax = [v.real,v2.real,v.imag*0.5,v2.imag*0.5].map(&:abs).max
+  step = (width * (vmax * time) / scale).ceil
   (0..step).each do |i|
     t = time * i.fdiv(step)
-    px = x + vx * t + ax * t * t / 2
-    py = y + vy * t + ay * t * t / 2
-    ix = (width / 2 + px / scale * width).round
-    iy = (height / 2 + py / scale * width / 2).round
+    q=p+v*t+a*t*t/2
+    ix = (width / 2 + q.real / scale * width).round
+    iy = (height / 2 + q.imag / scale * width / 2).round
     canvas[iy][ix] = 1 if (0...width).cover?(ix) && (0...height).cover?(iy)
   end
 end
 
 new_split_time = -> { 0.5 + rand }
-spark = lambda do |x, y, vx, vy, life, split_time, time|
+spark = lambda do |p, v, life, split_time, time|
   dt = [time, life, split_time].min
-  stroke.call x, y, vx, vy, wind, gravity, dt
-  x += vx * dt + wind * dt * dt / 2
-  y += vy * dt + gravity * dt * dt / 2
-  vx += wind * dt
-  vy += gravity * dt
+  stroke.call p,v,gravity,dt
+  p+=v*dt+gravity*dt*dt/2
+  v+=gravity*dt
   if dt == life
     []
   elsif dt == time
-    [[x, y, vx, vy, life - dt, split_time - dt]]
+    [[p, v, life - dt, split_time - dt]]
   else
     n = rand(5..10)
     n.times.map do
-      v = (0.5 + 0.4*rand) * 536 ** rand.i
-      spark.call x, y, vx + v.real, vy + v.imag, (life - dt) / 2 + 0.4 * rand, new_split_time.call, time - dt
+      v2 = (0.5 + 0.4*rand) * 536 ** rand.i
+      spark.call p, v+v2, (life - dt) / 2 + 0.4 * rand, new_split_time.call, time - dt
     end.inject(:+)
   end
 end
@@ -70,7 +67,7 @@ sparks = []
   ay = -[ti.fdiv(n), 1, [(3*n+2*m-3.0*ti)/m,0].max].min
   ax = -ay*0.1
   cx = ax
-  cy = ay + (ti > n ? gravity * (dt * (ti - n)) ** 2 / 2 : 0)
+  cy = ay + (ti > n ? gravity.imag * (dt * (ti - n)) ** 2 / 2 : 0)
   ir = (2 * r / scale * width).ceil
   iy = (cy / scale * width / 2).ceil
   (iy-ir/2...[iy+ir/2,height/2].min).each do |iy|
@@ -82,11 +79,12 @@ sparks = []
     end
   end
   ytop = -scale * height / width
-  stroke.call(-ytop*0.1, ytop, -0.1,1,0,0, ay-ytop-r/2)
+  stroke.call(ytop*(-0.1+1i)-0.02,-0.1+1i,0,ay-ytop-r/2)
+  stroke.call(ytop*(-0.1+1i)+0.02,-0.1+1i,0,ay-ytop-r/2)
   rand(2..8).times do
     next if rand > (n-ti).fdiv(n)*2
     a = [0.2+ti*0.05,1].min
-    sparks << [*(cx+cy.i).rect, *((1-a)*(-0.05+0.5.i) + a * (0.5 + 0.5*rand) * 536**rand.i).rect, 1 + rand, (1-a)+new_split_time.call]
+    sparks << [cx+cy.i,(1-a)*(-0.05+0.5i) + a * (0.5 + 0.5*rand) * 536**rand.i, 1 + rand, (1-a)+new_split_time.call]
   end if ti < n
   sparks = sparks.flat_map do |s|
     spark.call(*s, dt)
