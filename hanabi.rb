22ac0e2bc5a168633e6454721dc7c881bae1ac37
tompng <tomoyapenguin@gmail.com>
2020-08-26 04:24:43 +0900

    more compact

--- hanabi.rb
+++ hanabi.rb
@@ -1,75 +1,67 @@
 chars = ' ▘▝▀▖▌▞▛▗▚▐▜▄▙▟█'
-size=160
-canvas = (size / 2).times.map { [0] * size }
-show = lambda do
-  lines = (size / 4).times.map do |j|
-    (size / 2).times.map do |i|
-      chars[4.times.map{|k|canvas[2*j+k/2][2*i+k%2]<<k}.sum]
-    end.join
-  end
-  puts "\e[1;1H" + lines.join("\n")
-end
+width=160
+canvas = (width / 2).times.map { [0] * width }
 
-g = 0.5
+g = 0.5i
 scale = 4.0
-stroke = lambda do |p, v, a, time|
-  v2 = v + a * time
-  vmax = [v.real,v2.real,v.imag*0.5,v2.imag*0.5].map(&:abs).max
-  step = (size * (vmax * time) / scale).ceil
-  (0..step).each do
-    t = time * _1.fdiv(step)
-    q=p+v*t+a*t*t/2
-    ix = (size / 2 + q.real / scale * size).round
-    iy = (size / 4 + q.imag / scale * size / 2).round
-    canvas[iy][ix] = 1 if (0...size).cover?(ix) && (0...size/2).cover?(iy)
-  end
-end
+stroke = ->(p, v, a, t) {
+  v2 = v + a * t
+  n = (width * ((v+v.real).rect+(v2+v2.real).rect).map(&:abs).max * t / scale / 2 + 1).ceil
+  n.times {
+    u = t * _1.fdiv(n - 1)
+    q=p+v*u+a*u*u/2
+    (0...width).cover?(x = (width / 2 + q.real / scale * width).round) &&
+    (0...width/2).cover?(y = (width / 4 + q.imag / scale * width / 2).round) &&
+    canvas[y][x] = 1
+  }
+}
 
-spark = lambda do |p, v, lt, st, tt|
+spark = ->(p, v, lt, st, tt) {
   t = [tt, lt, st].min
-  stroke[p,v,g.i,t]
-  p+=v*t+g.i*t*t/2
-  v+=g.i*t
-  if t == lt
-    []
-  elsif t == tt
+  stroke[p,v,g,t]
+  p+=v*t+g*t*t/2
+  v+=g*t
+  (t == lt)?
+    []:
+  (t == tt)?
     [[p, v, lt - t, st - t]]
-  else
-    (0..rand(4..9)).flat_map do
-      w = (0.5 + 0.4*rand) * 536 ** rand.i
-      spark[p, v+w, (lt - t) / 2 + 0.4 * rand, 8, tt - t]
-    end
-  end
-end
+  :
+    (0..rand(4..9)).flat_map {
+      spark[p, v/4 + 536 ** rand.i*(1 + rand)/2, (lt - t) / 2 + 0.4 * rand, 8, tt - t]
+    }
+}
 
 sparks = []
-
-(0..).each do |i|
-  canvas.map { |l| size.times { l[_1] = 0 } }
+(0..).each {|i|
+  canvas.map { |l| width.times { l[_1] = 0 } }
   n = 200
   m = 40
   ti = i % (n+m)
   ti==0&&srand(0)
   t=[0,(3*n+2*m-3.0*ti)/m,1].sort[1]
-  dir=0.1-1i
-  burn = [ti.fdiv(n),t*t*(3-2*t)].min
-  ball = burn*dir + (ti > n ? g * (ti - n) ** 2 / 2.0 : 0).i
-  r = 0.12*[ti*0.05,1].min
-  ir = (2 * r / scale * size).ceil
-  iy0 = (ball.imag / scale * size / 2).ceil
-  (iy0-ir/2...iy0+ir/2).each do |iy|
-    iy < size/4 && (-ir..ir).each do |ix|
-      p = (ix +iy*2i)* scale / size
-      canvas[size/4+iy][size/2+ix]=1 if (p-ball).abs<r
-    end
-  end
-  [-1,1].map{stroke[dir*scale/2-0.02*_1,-dir,0,scale/2-burn-r/2]}
-  rand(2..8).times do
-    next if rand > (n-ti).fdiv(n)*2
+  d=0.1-1i
+  bt = [ti.fdiv(n),t*t*(3-2*t)].min
+  c = bt*d + (ti > n ? g * (ti - n) ** 2 / 2 : 0)
+  r = 0.1*[ti*0.05,1].min
+  ir = (r / scale * width).ceil
+  iy = (c.imag / scale * width / 2).ceil
+  (iy-ir...iy+ir).each { |y|
+    y < width/4 && (-2*ir..2*ir).each { |x|
+      p = (x +y*2i)* scale / width
+      canvas[width/4+y][width/2+x]=1 if (p-c).abs<r
+    }
+  }
+  [-1,1].map{stroke[d*scale/2-0.02*_1,-d,0,scale/2-bt-r/2]}
+  rand(2..8).times {
     a=[0.2+ti*0.05,1].min
-    sparks << [ball,-dir*(1-a)/2 + a * 0.5 * (1 + rand) * 536**rand.i, 1 + rand, (1-a)+0.5+rand]
-  end
+    sparks << [c,-d*(1-a)/2 + a * 0.5 * (1 + rand) * 536**rand.i, 1 + 2 * rand, (1-a)+0.5+rand] if (rand < 2.0*(n-ti)/n)
+  }
   sparks = sparks.flat_map{spark[*_1,1]}
-  show[]
+
+  puts "\e[1;1H" + (0...width / 4).map{|j|
+    (0...width / 2).map { |i|
+      chars[4.times.sum {|k| canvas[2*j+k/2][2*i+k%2]<<k }]
+    } * ''
+  }*$/
   sleep 0.1
-end
+}
