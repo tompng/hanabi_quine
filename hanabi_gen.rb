192c9ddcf970fd8b140741c7cc3856af65cc0919
tompng <tomoyapenguin@gmail.com>
2020-08-27 05:30:29 +0900

    margin: 8→9

--- hanabi_gen.rb
+++ hanabi_gen.rb
@@ -10,7 +10,7 @@ p (hanabi_code.chars-(32...32+64).map(&:chr)).uniq.sort.join
 
 
 header = <<~HEADER_CODE
-  eval($a=%(n=0x00;->z{$h='eval%c$a=%%%c'%[40,40]+$a;$z=41.chr*2+'[$z=%@'+$z;eval((';'*8+z+';'*2).scan(/^.{8}|.{8}$/)*'')}
+  eval($a=%(->z{n=0x00;$h='eval($a=%('+$a;$z=('))[$z=%@'+$z).lines;eval((';'*8+z+';'*2).scan(/^.{8}|.{8}$/)*'')}
   ))[$z=%@
 HEADER_CODE
 
@@ -37,25 +37,26 @@ end
 
 innercode = (hanabi_code+';'+10000.times.map{rand(10)}.join).chars.each_slice(72).take(47).map { '-' * 12 + _1.join + '-' * 12 }
 
-
-puts innercode
-
 94.times { |i|
   s = (0..47).map { j, k = coord(i, _1); innercode[j][k] } * ''
   inflated = rsobj.inflate(s[12..-1].tr('{|}', '"#!')).tr('"#!', '{|}')
   (0..47).each { j, k = coord(i, _1); innercode[j][k] = inflated[_1] }
 }
-original = innercode.map(&:dup)
-puts innercode
-640.times{
-  line = innercode.sample
-  line[rand line.size] = '█'
-}
-puts innercode
-$z = innercode.map{'-'*8+_1+'-'*8}.join("\n")
-
-restored = eval rs_code+';'+expand_code+';e'
 eval_code = "eval(e.map{_1[12,72].delete(32.chr)}*'')"
-side_code = eval [rs_code, expand_code, eval_code].join(';')
-puts restored
-puts restored == original
+side_code_chars = [rs_code, expand_code, eval_code].join(';').chars
+$z = innercode.map{'▄'*9+_1+'▄'*9}.join("\n").gsub(/▄/){side_code_chars.shift || ';'}.lines
+p side_code_chars.size
+
+
+zwas = $z.map(&:dup)
+640.times{
+  line = $z.sample
+  line[rand(9...9+96)] = '█'
+}
+puts zwas
+puts $z
+restored = eval rs_code+';'+expand_code
+# eval_code = "eval(e.map{_1[12,72].delete(32.chr)}*'')"
+# eval [rs_code, expand_code, eval_code].join(';')
+puts $z
+puts zwas == $z
