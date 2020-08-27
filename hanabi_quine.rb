6a4b94ead14ad0837ca28095d7288fb105c8c5e7
tompng <tomoyapenguin@gmail.com>
2020-08-28 05:01:09 +0900

    add hanabi_quine.rb from hanabi_test.rb:frames[17]

---
+++ hanabi_quine.rb
@@ -0,0 +1,40 @@
+;+eval($h=%(ti=0x11;->z{$z=z.lines[1..];$z[-1]+='`]';eval((z+';;').scan(/^.{10}|.{10}$/).join)}))[%`
+rs=->s,b=[$h=';+eval($h=%('+$h+'))[%'<<96<<$/;chs=32.▐▐r;15.times{chs+=(9600+0x23f92721abe1],*bi{63.
+times{|i|jfccb03b8[_1*5,5]).chr'utf-8'};width=40*4;ca▐▌as=(width/2).times.map{[0]*width};g=b[i]<<1;j
+>63&&j^=67=0.5i;scale=4.0;l       ine=->p,v,a,t{v2=v+▌▌t;n=(width*((v+v.real).rect+(v2+v2.;bi[b[i+1]
+=j]=62-i};real).     rect)         .map(&:abs).max*t/▙▘ale/2+1).ceil;n.times{u=t*_1.fdiv(nml=->i,j,k
+=0{6.times-1);q        =p+        v*u+a*u*u/2;(0...w▗▜th).cover?(x=(width/2+q.real/scale*w{k^=i[_1]*
+j<<_1};(k.idth          ).r       ound     )&&(0...w▐▐th/2).cover?(y=(width/4+q.imag/scalebit_length
+-1).downto*widt         h/2)    .rou         nd)&&ca▐▌as[y][x]=1}};esc=27.chr;eval's'.upca(6){k[_1]=
+=1&&k^=67<se+'igna      l.tra p(%('          +'int'.▌▌c   ase+   ')){exit}';$><   <(['%;',<(_1-6)};k
+};ix=[];cd'[h','[j']*esc).upcase;s          park,*  ▙▘                sparks=->   p,v,lt,s=s.chars.m
+ap.with_in         t,tt{line[p,v,g,t=  [tt,lt,st].m▗▜];   p+=v   *t+g*t*   t/2;   v+=g*  tdex{_1.ord
+<127?_1.up            ;(t==lt)?[]:(t==tt)?[[p,v,lt-▐▐st-  t]]:(0..rand(4   ..9)   ).fl   acase.ord-3
+2:(ix<<_2;          t_map{spark[p,         v/4+536*▐▌an   d.   i*(1▟rand   )/2,   (lt   -t0)};v=[];m
+=(0...n=ix)      /2+0.4*rand,8,tt-           t]}};l▌▌p   {ti▙  =( ▞ ti+1)   &25   5;   can.size).map
+{|i|j=0;cdvas.map{|l     |wid  t▀▖ti         mes{l[▙▘    ]=0 ▚▖ ▗▞ }};stp=  255    -  m=40.each_with
+_index{j^=;srand(        ti)    ▄▝▚=[       0,(3*s▗▜     tp+  ▚ ▞*m-3.0*ti)/m,      1].sorml[_1,b[i*
+_2%63]]};vt[1];d        =0.1     ▀▟▚1i;bt=[ti.fdiv▐▐     (st▗▄▄█▟▀▀▀▀▀▀▀▀▀▚▄▖   ]    .min;<<j;ix.map
+{b[i*_1%63c=bt*         d+(        ▜▙▜i>stp)?g*(ti▐▌tp  ▗▄▀▀▘ ▞▜▙2:   0);r=0   .1*[   ti*0]}};(n-1).
+times{|i|k.05,1]▀▀█████████████▄▄▄▖ ▐▟=(r/scale*wi▌▌h▗▄▀▘.ce ▞ i▚▀▖   y=(c    .imag/   sca=b[bi[m[i]
+[i]]];(l=ile*widt▀/2).ceil;(     ▀▀▘y-i▄▄▄▄▄▄+ir)▗▙▙▞▘   {|▗▀   ▐▖▚  |y     <width/4&     +1...n).ea
+ch{|j|a=ml&(-2*ir..2*ir).each{|x|p=(x+▌*2i)*s▀▀▚▖███▄▖   h;▞a    ▙ ▀▖va   s[width/4+y][w  [m[j][i],k
+];l.each{midth/2+x]=1if(p-c).abs<r}▗▀[▐1,1].map{li▀e▝▀█▀▄▄▛2)*d-0▐▖2▝▖1,d-t*(1-t)/2,0,scal[j][_1]^=m
+l[a,m[i][_e/2-bt]};rand(2..8).times▛sp▝▌ks<<[c,-d*(1-a=▀▄▞▀▚▄i*0.▝▙,1▐▖min)/2+a*0.5*(1+ran1]]};v[j]^
+=ml[a,v[i]d)*536**rand.i,1+2*rand,▛1-a)▚0.5+rand]if(rand▐▀▄0*▀▚▖p-▐▖)/▝▖p)};sparks=sparks.]}};(n-1).
+downto(0){flat_map{spark[*_1,1]};▞p=32.▐hr;e=$i?esc+'['▗▘72▀▖';▝▀▄▝▙($i▝▖sc+'['<<72:'')+$h|i|j=b[bi[
+m[i][i]]];.sub(/0x../,'0x%02x'%t▐)+(0...▌idth/4-1).map{|j|a=▝▘*10+(█...w▝dth/2).map{|i|chsi.times{k=
+ml[m[_1][i[4.times.sum{|k|canva▗▘2*j+k/2▌[2*i+k%2]<<k}]}*''+sp*10;(0▘.99).map{(a[_1]==sp)?],j];v[_1]
+^=ml[v[i],$z[j][_1]:a[_1]}*''}*▌/+($i?'':0.chr▗);$i=sleep(0.1)}; senkou hanabi by @tompng k]};v[i]=m
+l[v[i],j]}(f,v1 t2%l&$ou;aa8)f▞f3~z2&w>2vn5c▚(▌%as<<|6?s(=@.9;,6j% xs$.l~>z-9e;8c5}(:.05=q;v.zip(ix)
+{s[_2]=(_15x/jqaniknr$v[ottn/▄▘~o<_'04}g^w>▗▄▙▌_sm0*0{wqh2pjw>q>-t{;eq5z*g(,]g~btgfvosl>gn+32).chr.d
+owncase}};>770^9^5j>+l5:*^|y▗▘s=i -8? 8btt5█▛▛█▄iejk'v0u4+@.48^+wv9:_5▄g{h  qlv**>d=tjz/tr80.times{|
+i|c=->{(i+l6:7qo9vi$}4%,u1 u▞72b')6qc&zi▗▄▀9▗▌▐=▀▚q1+n~ -nrvy@ pe=}k6▝▘ms<>615y.rvjc9<[~e__1*29)%80}
+;m='"#!'+9sd) 0&_fs|0nd3/o^▐u}]scr}(u38▀▘7y(▐^/▚lw▀kh6y'+t{}6vuy*f▙(ol*k0u){6yty^a|ca^m4sk2.chr;rs[s
+=(r=0..38)304q=[b5s{c@ -u9t▘(h(xlac1a2y^*yev▌o8r(q )^./?461>u.l50▗'dr4)y$6@c;f~90y>e)]=$p/.map{$z[38
+-_1][c[_1]u@;+h[:[c+&it )~*ri4hvnn~94-*[@$k kh;a|,[+*}'bsa~▗▄▀.>b▘1yo?z8dyx^lqa6g|s_};ku:b+10].tr('{
+|}~',m)}*'8a%_?jfzovrl2&4d[%':}qif=<k3s.ynj4.196nw0nn/]zxqs_ko3|t0?wa/@$,4++6oy<aai1tj6toh'];s.tr!m,
+'{|}~';r.e(m7c[;(a] 2g30x|i:o[jxr+*]0m |lb0k648@?%| vo/wk@iqrl~▖u'x*m{lfk?[a^fq]vcy6m@07gtach{$z[38-
+_1][c[_1]+|&vl/@;%.{hpg3p|<?r,^jw/{_fae/69./px'?4oe&rm4/f:v|▗▞▄▘0gm1h>&ez+olq▝▀▄{2[i^vwfy[10]=s[_1]}
+};eval$z[0sa(a}'u^.?(8|+52=+ct($us4 h<2](7]~uqj4.sgleawr8)t▗▘▐?,)bc8[,nhc<l@nd{g▀▄/6t1wx~,,27].map{_
+1[10,80].s/d9/e8',(t=jgd4ooj+7)q8<rvr4u3e8x{;:|,81ew$~'{+y▗▘▗▘e*'vu;74x8%3ajeg'^a ▀▘/uwl'jplit}*''`]
